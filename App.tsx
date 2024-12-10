3b193c672c306ab1cd6f79019cbc0ecfa96eb590
Stefanie <stefaniebaarman@gmail.com>
2024-12-09 22:11:23 -0500

    json files for auth

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
+import React from 'react';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
   const isFirstTimeUser = true;
 
-  
   if (!isLoadingComplete) {
     return null;
   } else {
