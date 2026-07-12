29c9b9325426978b9588bb99ed898b114236dd4d
stefanie <stefaniebaarman@gmail.com>
2026-07-12 19:58:51 -0400

    Removed dead expo code

--- components/StyledText.tsx
+++
@@ -1,6 +0,0 @@
-import { Text, TextProps } from './Themed';
-import { fonts } from '@/constants/Variables';
-
-export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: fonts.medium }]} />;
-}
